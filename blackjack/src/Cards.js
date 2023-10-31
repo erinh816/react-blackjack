cfec96a3b156e311365fee402ec78534fe0d5dd0
JaeWon Jin <j1jin286@gmail.com>
2023-10-30 17:35:33 -0700

    Shows two random unique cards

--- blackjack/src/Cards.js
+++ blackjack/src/Cards.js
@@ -1,9 +1,25 @@
-const API_URL = "https://deckofcardsapi.com/api/deck/";
+import _ from "lodash";
 
-async function Cards() {
-  const response = await fetch(`${API_URL}/new/shuffle/?deck_count=1`);
-  const deck = await response.json();
-  console.log(deck);
+const API_URL = "https://deckofcardsapi.com/static/img/";
+
+function Cards() {
+  const suit = ["H", "S", "D", "C"];
+  const ranks = ["A", "2", "3","4", "5", "6", "7", "8", "9", "0", "J", "Q", "K"];
+
+  const firstCard = _.sample(ranks) + _.sample(suit);
+  let secondCard = _.sample(ranks) + _.sample(suit);
+
+  while(firstCard === secondCard){
+    secondCard = _.sample(ranks) + _.sample(suit);
+  }
+
+  return (
+    <div>
+      <img src={API_URL + firstCard + ".png"}></img>
+      <img src={API_URL + secondCard + ".png"}></img>
+    </div>
+  )
+
 };
 
 export default Cards;
